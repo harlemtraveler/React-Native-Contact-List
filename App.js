639cb822bcbabfed8d1aedf97caf2cf0ba624872
Timothy <timothy.keaveny@troyweb.com>
2019-02-04 16:07:00 -0500

    Remove excess code from App.js

--- App.js
+++ App.js
@@ -6,24 +6,3 @@ import Contacts from './screens/Contacts';
 export default function App() {
   return <Contacts />
 }
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Open up App.js to start working on your app!</Text>
-//         <Text>Changes you make will automatically reload.</Text>
-//         <Text>Shake your phone to open the developer menu.</Text>
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });

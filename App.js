b8db881ab91b990df136f06a58308800dd4eba46
Timothy <timothy.keaveny@troyweb.com>
2019-02-05 14:25:33 -0500

    Import "Profile" to test screen

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Contacts from './screens/Contacts';
+import Profile from './screens/Profile';
 
 export default function App() {
-  return <Contacts />
+  return <Profile />
 }

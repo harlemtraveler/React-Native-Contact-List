a1e3623a92272b6211adad0cac53265cb4c6d6ab
Timothy <timothy.keaveny@troyweb.com>
2019-02-06 13:41:49 -0500

    Import "AppNavigator" && render in App.js

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
+import AppNavigator from './routes';
 
 export default function App() {
-  return <Profile />
+  return <AppNavigator />
 }

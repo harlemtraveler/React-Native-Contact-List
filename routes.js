1938da0ba8d20704721d737c514b0cb182935a7b
Timothy <timothy.keaveny@troyweb.com>
2019-02-07 16:23:22 -0500

    Add nested nav using "TabNavigator" component from React Navigation

--- routes.js
+++ routes.js
@@ -1,10 +1,25 @@
-import { StackNavigator } from 'react-navigation';
+import {
+  TabNavigator,
+  StackNavigator,
+} from 'react-navigation';
+import React, { Component } from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 
+import Favorites from './screens/Favorites';
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
+import User from './screens/User';
 import colors from './utils/colors';
 
-export default StackNavigator(
+const getTabBarIcon = icon => ({ tintColor }) => (
+  <MaterialIcons
+    name={icon}
+    size={26}
+    style={{ color: tintColor }}
+  />
+);
+
+const ContactsScreens = StackNavigator(
   {
     Contacts: {
       screen: Contacts,
@@ -15,5 +30,67 @@ export default StackNavigator(
   },
   {
     initialRouteName: 'Contacts',
+    navigationOptions: {
+      tabBarIcon: getTabBarIcon('list'),
+    },
+  },
+);
+
+const FavoritesScreens = StackNavigator(
+  {
+    Favorites: {
+      screen: Favorites,
+    },
+    Profile: {
+      screen: Profile,
+    },
+  },
+  {
+    initialRouteName: 'Favorites',
+    navigationOptions: {
+      tabBarIcon: getTabBarIcon('star'),
+    },
+  },
+);
+
+const UserScreens = StackNavigator(
+  {
+    User: {
+      screen: User,
+    },
+  },
+  {
+    initialRouteName: 'User',
+    navigationOptions: {
+      tabBarIcon: getTabBarIcon('person'),
+    },
+  },
+);
+
+export default TabNavigator(
+  {
+    Contacts: {
+      screen: ContactsScreens,
+    },
+    Favorites: {
+      screen: FavoritesScreens,
+    },
+    User: {
+      screen: UserScreens,
+    },
+  },
+  {
+    initialRouteName: 'Contacts',
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      style: {
+        backgroundColor: colors.greyLight,
+      },
+      showLabel: false,
+      showIcon: true,
+      activeTintColor: colors.blue,
+      inactiveTintColor: colors.greyDark,
+      renderIndicator: () => null,
+    },
   },
 );

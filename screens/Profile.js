cd0ec3bb859d6690c27ecb16afa66c1151f81de3
Timothy <timothy.keaveny@troyweb.com>
2019-02-06 19:50:05 -0500

    Add screen specific navigation options to "Profile.js"

--- screens/Profile.js
+++ screens/Profile.js
@@ -7,6 +7,17 @@ import { fetchRandomContact } from '../utils/api';
 import colors from '../utils/colors';
 
 export default class Profile extends Component {
+  static navigationOptions = ({ navigation: { state: { params } } }) => {
+    const { contact: { name } } = params;
+    return {
+      title: name.split(' ')[0],
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: colors.blue,
+      },
+    };
+  };
+
   render() {
     const { navigation: { state: { params } } } = this.props;
     const { contact } = params;
